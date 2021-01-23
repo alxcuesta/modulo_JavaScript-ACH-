00049b5206ea799eec77776c6952e80cd29975e8
alxcuesta <77157070+alxcuesta@users.noreply.github.com>
2021-01-23 11:46:54 +0100

    primera subida_JS2

---
+++ ejercicio_2/script.js
@@ -0,0 +1,16 @@
+//prueba random de funcionamiento
+console.log ('hola caracola')
+
+//hacer un objeto con dos características
+const generic = 'Mi cumpleaños es el día:'
+const alex = {
+    dia:5,
+    mes: 'Junio'
+}
+console.log ( generic + " " +alex.dia + ' de ' + alex.mes)
+
+//añadir un array con los meses del año
+
+const textogenerico = 'y el mes siguiente es:' 
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+console.log ( textogenerico + " " + meses[6])
